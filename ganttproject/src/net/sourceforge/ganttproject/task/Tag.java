1faf469f61fa9231fb706a38a27679e2f10984dd
Joana Rita Cruz <jrf.cruz@campus.fct.unl.pt>
2022-11-29 13:42:52 +0000

    changes

--- ganttproject/src/net/sourceforge/ganttproject/task/Tag.java
+++ ganttproject/src/net/sourceforge/ganttproject/task/Tag.java
@@ -2,15 +2,32 @@ package net.sourceforge.ganttproject.task;
 import net.sourceforge.ganttproject.task.Task;
 
 import java.util.Iterator;
-
+/*
+Represents a tag that allows us to filter tasks
+ */
 public interface Tag {
+    /*
+    Returns tag name
+     */
     public String getTagName();
 
+    /*
+    Adds a new task to this tag
+     */
     public boolean tagTask(Task task);
 
+    /*
+    Removes a task from tag
+     */
     public boolean removeTaskFromTag(Task task);
 
+    /*
+    Shows all tasks with this tag
+     */
     public Iterator<Task> getTaggedTasks();
 
+    /*
+    Gets number of tasks with this tag
+     */
     public int getNumberOfTaggedTasks();
 }
