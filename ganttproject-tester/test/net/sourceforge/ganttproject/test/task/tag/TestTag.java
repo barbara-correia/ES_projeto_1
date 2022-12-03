929065c43f3aa0fbba88a452c83dc450f21815d7
Joana Rita Cruz <jrf.cruz@campus.fct.unl.pt>
2022-12-03 17:08:39 +0000

    Finished and fixed test class (Goncalo Gomes)

--- ganttproject-tester/test/net/sourceforge/ganttproject/test/task/tag/TestTag.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/test/task/tag/TestTag.java
@@ -6,6 +6,7 @@ import net.sourceforge.ganttproject.task.*;
 import net.sourceforge.ganttproject.TestSetupHelper;
 import org.junit.Test;
 
+import java.awt.*;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,8 +14,8 @@ import java.util.List;
 public class TestTag {
     @Test
     public void addTasks(){
-        Tag a = new TagImpl("a");
-        Tag b = new TagImpl("b");
+        Tag a = new TagImpl("a", Color.RED);
+        Tag b = new TagImpl("b", Color.BLUE);
 
         GanttCalendar calendar = TestSetupHelper.newMonday();
         TaskManagerImpl manager = (TaskManagerImpl) TestSetupHelper.newTaskManagerBuilder().build();
@@ -51,8 +52,8 @@ public class TestTag {
 
     @Test
     public void removeTasks(){
-        Tag a = new TagImpl("a");
-        Tag b = new TagImpl("b");
+        Tag a = new TagImpl("a", Color.RED);
+        Tag b = new TagImpl("b", Color.BLUE);
 
         GanttCalendar calendar = TestSetupHelper.newMonday();
         TaskManagerImpl manager = (TaskManagerImpl) TestSetupHelper.newTaskManagerBuilder().build();
@@ -104,4 +105,87 @@ public class TestTag {
         assert(testB.get(0).equals(o));
 
     }
+
+    @Test
+    public void testTagManager(){
+        Tag a = new TagImpl("a", Color.BLUE);
+        Tag b = new TagImpl("b", Color.RED);
+
+
+        GanttCalendar calendar = TestSetupHelper.newMonday();
+        TaskManagerImpl manager = (TaskManagerImpl) TestSetupHelper.newTaskManagerBuilder().build();
+
+        TagManager tManager = new TagManagerImpl();
+        assert(tManager.getNumberOfTags() == 0);
+
+        Task z = new GanttTask("z",calendar,10L,manager,-1);
+        Task w = new GanttTask("z",calendar,10L,manager,-1);
+        Task y = new GanttTask("z",calendar,10L,manager,-1);//
+        Task u = new GanttTask("z",calendar,10L,manager,-1);
+        Task v = new GanttTask("z",calendar,10L,manager,-1);
+        Task o = new GanttTask("z",calendar,10L,manager,-1);
+
+        tManager.addTag(a);
+
+        assert(tManager.getNumberOfTags() == 1);
+
+        tManager.removeTag(a.getTagName());
+
+        assert (tManager.getNumberOfTags() == 0);
+
+        tManager.addTag(a);
+        tManager.addTag(b);
+
+        assert (tManager.getNumberOfTags() == 2);
+
+        tManager.addTaskToTag(a.getTagName(), z);
+
+        Iterator<Task> itA = a.getTaggedTasks();
+        List<Task> testA = new LinkedList<>();
+        while(itA.hasNext()) {
+            Task t = itA.next();
+            testA.add(t);
+        }
+
+        assert(a.getNumberOfTaggedTasks() == 1);
+        assert(testA.get(0).equals(z));
+
+        tManager.addTaskToTag(a.getTagName(), w);
+        assert (a.getNumberOfTaggedTasks() == 2);
+
+        a.removeTaskFromTag(w);
+        a.removeTaskFromTag(z);
+
+        assert(a.getNumberOfTaggedTasks() == 0);
+
+
+        tManager.addTaskToTag(b.getTagName(), y);
+
+        Iterator<Task> it2 = tManager.getTasksByTag(b);
+        int i = 0;
+        while(it2.hasNext()){
+            i++;
+            it2.next();
+        }
+        assert(i == 1);
+
+        tManager.addTaskToTag(b.getTagName(), u);
+        it2 = tManager.getTasksByTag(b);
+        i = 0;
+        while(it2.hasNext()){
+            i++;
+            it2.next();
+        }
+        assert(i == 2);
+
+        it2 = tManager.getTasksByTag(b.getTagName());
+        i = 0;
+        while(it2.hasNext()){
+            i++;
+            it2.next();
+        }
+        assert(i == 2);
+
+    }
+
 }
